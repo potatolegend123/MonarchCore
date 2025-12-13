c7504f9d43f9944301107d9847e76de1d85a1792
poop <potatogod173@gmail.com>
2025-12-13 03:12:40 -0600

    commit

--- Main/Main.lua
+++ Main/Main.lua
@@ -1,11 +1,11 @@
-loadstring(game:HttpGet(REPO_UTILITY .. "Services.lua"))()
-
 local REPO = "https://raw.githubusercontent.com/potatolegend123/MonarchCore/refs/heads/main/"
 local REPO_UTILITY = REPO .. "Utility/"
 local REPO_GAMES = REPO .. "Games/"
 local REPO_LIBRARY = REPO .. "Library/"
 local REPO_DEBUG = REPO .. "Debug/"
 local REPO_MAIN = REPO .. "Main/"
+
+loadstring(game:HttpGet(REPO_UTILITY .. "Services.lua"))()
 
 Trove = loadstring(game:HttpGet(REPO_UTILITY .. "Trove.lua"))()
 
