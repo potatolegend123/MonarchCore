3505daa0ae6f4606543afcb1ef50bdd64adbdf3f
poop <potatogod173@gmail.com>
2025-12-13 03:21:03 -0600

    commit

--- Main/Loader.lua
+++ Main/Loader.lua
@@ -8,6 +8,9 @@ repeat wait() until game:IsLoaded()
 
 local REPO = "https://raw.githubusercontent.com/potatolegend123/MonarchCore/refs/heads/main/"
 local REPO_UTILITY = REPO .. "Utility/"
+local REPO_GAMES = REPO .. "Games/"
+local REPO_LIBRARY = REPO .. "Library/"
+local REPO_DEBUG = REPO .. "Debug/"
 local REPO_MAIN = REPO .. "Main/"
 
 local HttpService = game:GetService("HttpService")
